522fa4d7d006094a183e359fd2dc5ae098913f4d
Daniel Garon <dgaron@colostate.edu>
2024-12-05 13:38:52 -0700

    Clean up part1 for part2

--- day2/part1.cc
+++ day2/part1.cc
@@ -7,40 +7,45 @@
 
 #include "bobo_utils.h"
 
+#define MAX_DISTANCE 3
+
+bool is_level_safe(const std::vector<int>& level) {
+    bool safe = true;
+    bool increasing = true;
+    bool decreasing = true;
+    for (std::size_t i = 0; i < level.size()-1; ++i) {
+        if (level[i] < level[i+1]) {
+            decreasing = false;
+        } else if (level[i] > level[i+1]) {
+            increasing = false;
+        } else if (level[i] == level[i+1]) {
+            return false;
+        }
+        if (abs(level[i] - level[i+1]) > MAX_DISTANCE) {
+            safe = false;
+        }
+    }
+    safe &= (increasing ^ decreasing);
+    return safe;
+}
+
 int main() {
 
     std::vector<std::string> raw_data = readFile("input.txt");
     
     int result = 0;
-    const int MAX_DISTANCE = 3;
 
     std::istringstream iss;
     for (const auto &s : raw_data) {
-        std::vector<int> current;
+        std::vector<int> current_level;
         int n;
         iss.str(s);
         while(iss >> n) {
-            current.push_back(n);
+            current_level.push_back(n);
         }
 
-        bool safe = true;
-        std::size_t lt_count = 0;
-        std::size_t gt_count = 0;
-        for (std::size_t i = 1; i < current.size(); ++i) {
-            if (current[i] < current[i-1]) {
-                ++lt_count;
-            } else if (current[i] > current[i-1]) {
-                ++gt_count;
-            }
-            if (abs(current[i] - current[i-1]) > MAX_DISTANCE) {
-                safe = false;
-            }
-        }
-
-        if (lt_count != current.size()-1 && gt_count != current.size()-1) {
-            safe = false;
-        }
-
+        bool safe = is_level_safe(current_level);
+        // result += safe; is too ugly
         if (safe) {
             ++result;
         }
